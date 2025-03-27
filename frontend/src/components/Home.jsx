02c69e3aef29bbbc680cdc2e01bcbc48989f922e
harukicode <sparky.doto@gmail.com>
2025-03-27 15:09:01 +0100

    customize home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,14 +1,18 @@
 import { useNavigate } from "react-router-dom";
+import styles from "./Home.module.css";
 
 export const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <div>
-        <h1>Hello in simple chat</h1>
-        <button onClick={() => navigate("login")}>Get Started</button>
-      </div>
+    <div className={styles.container}>
+      <h1 className={styles.container__greeting}>Hello in simple chat</h1>
+      <button
+        className={styles.container__button}
+        onClick={() => navigate("login")}
+      >
+        Get Started
+      </button>
     </div>
   );
 };
