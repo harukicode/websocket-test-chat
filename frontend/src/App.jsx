1790d673c47afa4bc5f0c1003d82116de7b47a01
harukicode <sparky.doto@gmail.com>
2025-03-27 10:37:43 -0700

    add styles for login form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LogginScreen } from "./components/LogginScreen";
-import { ChatScreen } from "./components/ChatScreen/ChatScreen";
-import { Home } from "./components/Home";
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { LoginScreen } from './components/LoginScreen.jsx';
+import { ChatScreen } from './components/ChatScreen/ChatScreen';
+import { Home } from './components/Home';
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="login" element={<LogginScreen />} />
+          <Route path="login" element={<LoginScreen />} />
           <Route path="chat" element={<ChatScreen />} />
         </Routes>
       </BrowserRouter>
---
+++ frontend/src/components/LoginScreen.jsx
@@ -0,0 +1,51 @@
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import styles from "./LoginScreen.module.css";
+
+export const LoginScreen = () => {
+  const { register, handleSubmit } = useForm();
+  const navigation = useNavigate();
+
+  const onSubmitHandler = (data) => {
+    navigation("/chat");
+
+    console.log(data);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.login_form__container}>
+        <h2 className={styles.login_form__title}>Login</h2>
+        <form
+          className={styles.login_form}
+          onSubmit={handleSubmit(onSubmitHandler)}
+        >
+          <label>
+            Username:
+            <input
+              className={styles.login_form__username}
+              {...register("username", {
+                required: true,
+              })}
+            />
+          </label>
+
+          <label>
+            Password:
+            <input
+              className={styles.login_form__password}
+              type="password"
+              {...register("password", {
+                required: true,
+              })}
+            />
+          </label>
+
+          <button className={styles.login_form__button} type="submit">
+            Login In
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
