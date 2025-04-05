1a5c2fe62731bbefe6e7931e231597a1c11c4127
harukicode <sparky.doto@gmail.com>
2025-04-05 18:54:31 +0200

    connect backend and refactoring models

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -2,33 +2,33 @@ const db = require("../database");
 
 class Message {
   static create(senderId, receiverId, content) {
-    return new Promise((resolve, reject) => {
-      db.run(
-        `INSERT INTO messages (sender_id, receiver_id, content) VALUES (?, ?, ?)`,
-        [senderId, receiverId, content],
-        function (err) {
-          if (err) {
-            reject(err);
-          } else {
-            db.get(
-              `SELECT m.*, u.username as sender_username
-                    FROM messages m
-                    JOIN users u ON m.sender_id = u.id
-                    WHERE m.id = ?`,
-              [this.lastID],
-              (err, message) => {
-                if(err) {
-                  reject(err);
-                } else {
-                  resolve(message);
-                }
+  return new Promise((resolve, reject) => {
+    db.run(
+      `INSERT INTO messages (sender_id, received_id, content) VALUES (?, ?, ?)`,
+      [senderId, receiverId, content],
+      function (err) {
+        if (err) {
+          reject(err);
+        } else {
+          db.get(
+            `SELECT m.*, u.username as sender_username
+                  FROM messages m
+                  JOIN users u ON m.sender_id = u.id
+                  WHERE m.id = ?`,
+            [this.lastID],
+            (err, message) => {
+              if(err) {
+                reject(err);
+              } else {
+                resolve(message);
               }
-            )
-          }
+            }
+          )
         }
-      )
-    })
-  }
+      }
+    )
+  })
+}
   
   static getConversation(userId1, userId2, limit = 50) {
     return new Promise((resolve, reject) => {
